d104842b85caf158d98a799bc085f44fa91292ff
Ilja von Hoessle <4417345+iljabvh@users.noreply.github.com>
2026-03-05 23:43:19 +0100

    docs: update copyright year to 2026
    - canister.rs and ic_types.rs: Copyright 2025 → 2026
    
    Signed-off-by: Ilja von Hoessle <4417345+iljabvh@users.noreply.github.com>

--- src/ic_types.rs
+++ src/ic_types.rs
@@ -1,4 +1,4 @@
-//  Copyright 2025 PolyCrypt GmbH
+//  Copyright 2026 PolyCrypt GmbH
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
